463db40227e1a4083d154b880b612772270ed3fc
GabrieldoCarmo <gabriel_o.carmo@hotmail.com>
2019-03-12 01:49:43 -0300

    Commt inicial

---
+++ src/main/java/AcademiaGestaoWebApi/AcademiaGestaoWebApiApplication.java
@@ -0,0 +1,13 @@
+package AcademiaGestaoWebApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaGestaoWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaGestaoWebApiApplication.class, args);
+	}
+
+}
