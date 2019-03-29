1a737d325fc4b639d88b35c6bde6c6aff8ae7009
GabrieldoCarmo <gabriel_o.carmo@hotmail.com>
2019-03-29 00:55:44 -0300

    Security

--- src/main/java/AcademiaGestaoWebApi/AcademiaGestaoWebApiApplication.java
+++ src/main/java/AcademiaGestaoWebApi/AcademiaGestaoWebApiApplication.java
@@ -3,8 +3,6 @@ package AcademiaGestaoWebApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import AcademiaGestaoWebApi.Config.ConnectionConfig;
-
 @SpringBootApplication
 public class AcademiaGestaoWebApiApplication {
 
