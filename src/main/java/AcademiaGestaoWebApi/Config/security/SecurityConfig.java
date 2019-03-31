afc8b2a2148c496b31430a287f6bfa9b179a5f6d
GabrieldoCarmo <gabriel_o.carmo@hotmail.com>
2019-03-31 00:55:11 -0300

    swagger security

--- src/main/java/AcademiaGestaoWebApi/Config/security/SecurityConfig.java
+++ src/main/java/AcademiaGestaoWebApi/Config/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,11 +39,22 @@ public class SecurityConfig<CustomizeAuthenticationSuccessHandler> extends WebSe
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
             .authorizeRequests()
-                .antMatchers("/oauth/token").permitAll()
+                .antMatchers("swagger**").permitAll()
+                .antMatchers("/oauth/token").permitAll()                
                 .anyRequest().authenticated()
                 .and()
             .httpBasic()
                 .and()
             .csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs",
+                                   "/configuration/ui",
+                                   "/swagger-resources/**",
+                                   "/configuration/security",
+                                   "/swagger-ui.html",
+                                   "/webjars/**");
     }
 }
