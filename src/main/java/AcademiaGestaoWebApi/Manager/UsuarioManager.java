ba0bfed05a00286bb91cbf180ae1cfd379907ae3
GabrielolCarmo <gabriel_o.carmo@hotmail.com>
2019-10-23 02:05:51 -0300

    Usuario logado

--- src/main/java/AcademiaGestaoWebApi/Manager/UsuarioManager.java
+++ src/main/java/AcademiaGestaoWebApi/Manager/UsuarioManager.java
@@ -1,11 +1,50 @@
 package AcademiaGestaoWebApi.Manager;
 
+import AcademiaGestaoWebApi.Config.ConnectionConfig;
+import AcademiaGestaoWebApi.Models.Aluno;
+import AcademiaGestaoWebApi.Models.Usuario;
 import AcademiaGestaoWebApi.Repository.UsuarioRepository;
+import java.sql.Connection;
+import java.util.List;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UsuarioManager {
     
-    public boolean atualizarSenha(String senha, String usuario) throws Exception {        
+    private final Connection connection;
+    private final AlunoManager alunoManager;
+
+    public UsuarioManager() {
+        connection = ConnectionConfig.getConnection(false);
+        alunoManager = new AlunoManager();
+    }        
+
+    public Usuario obterUsuario(String userName) throws Exception {
+        UsuarioRepository repositori = new UsuarioRepository();
+        try {
+            Usuario usuario = repositori.obterUsuario(userName, connection);
+            
+            if (usuario == null || usuario.getIdAluno() == null){
+                connection.commit();
+                ConnectionConfig.closeConnection(connection);
+                return usuario;
+            }
+            
+            List<Aluno> alunos = alunoManager.selectAlunos(usuario.getIdAluno());
+            Aluno aluno = alunos.stream().findFirst().get();
+            
+            usuario.setAluno(aluno);
+            
+            connection.commit();
+            ConnectionConfig.closeConnection(connection);
+            return usuario;
+        } catch (Exception ex) {
+            connection.rollback();
+            ConnectionConfig.closeConnection(connection);
+            throw ex;
+        }
+    }
+
+    public boolean atualizarSenha(String senha, String usuario) throws Exception {
         String senhaEncrypt = new BCryptPasswordEncoder().encode(senha);
         UsuarioRepository repositori = new UsuarioRepository();
         try {
@@ -13,6 +52,6 @@ public class UsuarioManager {
             return result;
         } catch (Exception ex) {
             throw ex;
-        } 
-    }   
+        }
+    }
 }
